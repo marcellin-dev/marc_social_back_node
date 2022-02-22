0c12b5f3607a1d11ebff31dbf82545f1e5696d9d
marcellin-dev <lelemarcellin@gmail.com>
2022-02-22 12:48:49 +0100

    change cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: '*',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   'allowedHeaders': ['sessionId', 'Content-Type'],
   'exposedHeaders': ['sessionId'],
