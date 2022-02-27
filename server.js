afa3af0cd65c8c4e6abd3269b9f5069101aee7bb
marcellin-dev <lelemarcellin@gmail.com>
2022-02-27 21:28:50 +0100

    change client url add allow all

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: '*',//process.env.CLIENT_URL,
   credentials: true,
   'allowedHeaders': ['sessionId', 'Content-Type'],
   'exposedHeaders': ['sessionId'],
