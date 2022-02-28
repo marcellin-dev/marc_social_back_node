85fd01ea8839c07bea0f4caf420bcf7777e93b28
marcellin-dev <lelemarcellin@gmail.com>
2022-03-01 00:20:21 +0100

    change pass bd

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { signUpErrors, signInErrors } = require('../utils/errors.utils');
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
-
+//token
 const createToken = (id) => {
   return jwt.sign({id}, process.env.TOKEN_SECRET, {
     expiresIn: maxAge
